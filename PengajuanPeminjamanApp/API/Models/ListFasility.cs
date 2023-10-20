d7fbebc7eda62faab97d7dc40022ced570c30d44
YolendesHermadiDandi <marselius306@gmail.com>
2023-10-20 15:52:53 +0700

    add database and migration

--- PengajuanPeminjamanApp/API/Models/ListFasility.cs
+++ PengajuanPeminjamanApp/API/Models/ListFasility.cs
@@ -8,7 +8,7 @@ public class ListFasility : GeneralModel
 	[Column("request_guid")]
 	public Guid RequestGuid { get; set; }
 	[Column("fasility_guid")]
-	public string FasilityGuid { get; set;}
+	public Guid FasilityGuid { get; set;}
 	[Column("total_fasility")]
 	public int TotalFasility { get; set;}
 
