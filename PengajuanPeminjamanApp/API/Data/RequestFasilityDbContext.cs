0b3ae966fc418e8ca6d29bf59c67928046677235
Afifudin <afifudin.card@gmail.com>
2023-10-20 14:35:17 +0700

    Add DbContext

---
+++ PengajuanPeminjamanApp/API/Data/RequestFasilityDbContext.cs
@@ -0,0 +1,25 @@
+﻿using API.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data;
+
+public class RequestFasilityDbContext : DbContext
+{
+    public RequestFasilityDbContext(DbContextOptions<RequestFasilityDbContext> options) : base(options){}
+
+    public DbSet<Account> Accounts { get; set; }
+    public DbSet<AccountRole> AccountRoles { get; set; }
+    public DbSet<Employee> Employees { get; set; }
+    public DbSet<Fasility> Fasilities { get; set; }
+    public DbSet<ListFasility> ListFasilities { get; set; }
+    public DbSet<Notification> Notifications { get; set; }
+    public DbSet<Request> Requests { get; set; }
+    public DbSet<Role> Roles { get; set; }
+    public DbSet<Room> Rooms { get; set; }
+
+	protected override void OnModelCreating(ModelBuilder modelBuilder)
+	{
+		
+	}
+
+}
